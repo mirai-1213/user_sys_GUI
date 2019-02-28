d56e1617b5d80e45e59bb40ff452db1b073b6d27
mirai-1213 <wl951213@gmail.com>
2019-03-01 00:12:11 +0900

    格式优化

--- user_sys_GUI/login_main.py
+++ user_sys_GUI/login_main.py
@@ -1,14 +1,15 @@
-import login_GUI as G
+import user_sys_GUI.login_GUI as gui
+
 
 # 创建新用户
 def new_user():
-    fieldValues = G.new_user_window()
-    for count in range(len(fieldValues)):
-        if fieldValues[count] == "":
-            G.error_msg("creat_user_info_none")
+    field_values = gui.new_user_window()
+    for count in range(len(field_values)):
+        if field_values[count] == "":
+            gui.error_msg("creat_user_info_none")
             return 0
-    if fieldValues[1] != fieldValues[2]:
-        G.error_msg("passwords are different")
+    if field_values[1] != field_values[2]:
+        gui.error_msg("passwords are different")
         
     return 1
 
@@ -22,8 +23,9 @@ def old_user():
 def exit_pro():
     return 0
 
+
 def main():
-    user_type = G.main_window()
+    user_type = gui.main_window()
     # user_type return is 'str'
     if user_type == "老用户":
         old_user()
@@ -34,5 +36,6 @@ def main():
     elif user_type == "退出":
         exit_pro()
         return 0
+
 
 main()
